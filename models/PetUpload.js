161e7424afeb06f11b2b7fc525fabfdfb3c8f4aa
Rethaniel Ramos <rethanielramos@gmail.com>
2022-06-17 03:26:46 +0800

    fix PetUpload model error

--- models/PetUpload.js
+++ models/PetUpload.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const mongooseDelete = require("mongoose-delete");
 
-const uploadSchema = new mongoose.Schema(
+const petUploadSchema = new mongoose.Schema(
   {
     pet_id: { type: mongoose.Schema.Types.ObjectId, ref: "Pet" },
     upload_id: { type: mongoose.Schema.Types.ObjectId, ref: "Upload" },
@@ -9,8 +9,8 @@ const uploadSchema = new mongoose.Schema(
   { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
-uploadSchema.plugin(mongooseDelete, { deletedAt: true });
+petUploadSchema.plugin(mongooseDelete, { deletedAt: true });
 
-const Upload = mongoose.model("Upload", uploadSchema);
+const PetUpload = mongoose.model("PetUpload", petUploadSchema);
 
-module.exports = Upload;
+module.exports = PetUpload;
