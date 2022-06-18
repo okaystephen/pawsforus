0049e775a9bb4b925396d81bae4249e9ca534932
Rethaniel Ramos <rethanielramos@gmail.com>
2022-06-18 18:48:48 +0800

    show 3 recently created pets in profile page

--- controllers/web/profile-controller.js
+++ controllers/web/profile-controller.js
@@ -1,11 +1,24 @@
+const Pet = require("../../models/Pet");
+// const {ObjectId} = require('mongoose')
+
 const profileController = {
-  show: (req, res) => {
-    const { user } = req;
-    res.render("profile", {
-      layout: false,
-      // TODO: also return pets and analytics
-      data: { user },
-    });
+  show: async (req, res) => {
+    try {
+      const { user } = req;
+      const pets = await Pet.find({ owner_id: user._id })
+        .limit(3)
+        .sort({ created_at: -1 })
+        .populate("uploads")
+        .lean()
+        .exec();
+
+      res.render("profile", {
+        layout: false,
+        data: { user, pets },
+      });
+    } catch (error) {
+      res.status(500).send(error);
+    }
   },
 };
 
