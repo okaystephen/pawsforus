6fc5935854c1619cb7c54d0cf5a3cd8d7c90eeef
Rethaniel Ramos <rethanielramos@gmail.com>
2022-06-18 16:22:38 +0800

    allow switching of public driver using PUBLIC_DRIVER env variable

--- config/filesystems.js
+++ config/filesystems.js
@@ -1,6 +1,5 @@
-const appConfig = require("./app");
-
 module.exports = {
+  publicDriver: process.env.PUBLIC_DRIVER || "local",
   firebase: {
     bucketName: process.env.FIREBASE_BUCKET_NAME,
     projectId: process.env.FIREBASE_PROJECT_ID,
--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -3,39 +3,43 @@ const FirebaseStorage = require("multer-firebase-storage");
 const filesystemsConfig = require("../config/filesystems");
 const appConfig = require("../config/app");
 
-const localPublicStorage = multer.diskStorage({
-  destination: `storage/app/public/`,
-  filename: function (req, file, cb) {
-    const filename = `${Date.now()}.${file.mimetype.split("/")[1]}`;
-    
-    file.publicUrl = new URL(
-      `storage/${filename}`,
-      `${appConfig.url}:${appConfig.port}`
-    ).toString();
+const localPublicStorage = () =>
+  multer.diskStorage({
+    destination: `storage/app/public/`,
+    filename: function (req, file, cb) {
+      const filename = `${Date.now()}.${file.mimetype.split("/")[1]}`;
 
-    cb(null, filename);
-  },
-});
+      file.publicUrl = new URL(
+        `storage/${filename}`,
+        `${appConfig.url}:${appConfig.port}`
+      ).toString();
 
-const cloudPublicStorage = FirebaseStorage({
-  bucketName: filesystemsConfig.firebase.bucketName,
-  credentials: {
-    projectId: filesystemsConfig.firebase.projectId,
-    privateKey: filesystemsConfig.firebase.privateKey,
-    clientEmail: filesystemsConfig.firebase.clientEmail,
-  },
-  directoryPath: "public",
-  public: true,
-  unique: true,
-  hooks: {
-    afterUpload: (req, file, fileRef, bucketRef) => {
-      file.filename = fileRef.id;
-    }
-  }
-});
+      cb(null, filename);
+    },
+  });
+
+const cloudPublicStorage = () =>
+  FirebaseStorage({
+    bucketName: filesystemsConfig.firebase.bucketName,
+    credentials: {
+      projectId: filesystemsConfig.firebase.projectId,
+      privateKey: filesystemsConfig.firebase.privateKey,
+      clientEmail: filesystemsConfig.firebase.clientEmail,
+    },
+    directoryPath: "public",
+    public: true,
+    unique: true,
+    hooks: {
+      afterUpload: (req, file, fileRef, bucketRef) => {
+        file.filename = fileRef.id;
+      },
+    },
+  });
+
+const publicStorages = { local: localPublicStorage, cloud: cloudPublicStorage };
 
 const fileUpload = multer({
-  storage: cloudPublicStorage,
+  storage: publicStorages[filesystemsConfig.publicDriver](),
   preservePath: true,
 });
 
