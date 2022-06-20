b44cb7141a43e53d02a0d467ff47a01ac7abf800
Rethaniel Ramos <rethanielramos@gmail.com>
2022-06-21 00:26:21 +0800

    implement matchValidator.createMatchValidator

---
+++ validators/match-validator.js
@@ -0,0 +1,27 @@
+const { body } = require("express-validator");
+const Match = require("../models/Match");
+
+const hasNotYetLikedThisPet = async (value, { req, location, path }) => {
+  const match = await Match.findOne({
+    pet_id: req.body.pet_id,
+    liked_pet_id: req.body.liked_pet_id,
+  })
+    .lean()
+    .exec();
+
+  if (match) throw new Error("Your pet has already liked this pet.");
+
+  return true;
+};
+
+const matchValidator = {
+  createMatchValidator: [
+    body("pet_id", "Pet ID is required").exists().isMongoId(),
+    body("liked_pet_id", "Liked Pet ID is required")
+      .exists()
+      .isMongoId()
+      .custom(hasNotYetLikedThisPet),
+  ],
+};
+
+module.exports = matchValidator;
