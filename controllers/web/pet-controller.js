6eb5161f1ce686aed746e584671b829073a11daa
Ira Villanueva <ira_villanueva@dlsu.edu.ph>
2022-06-21 19:45:43 +0800

    added edit pet profile (pics not included yet)

--- controllers/web/pet-controller.js
+++ controllers/web/pet-controller.js
@@ -25,39 +25,39 @@ const petController = {
 
     // if no errors
     const {
-      pet_name,
-      pet_about,
-      pet_birth,
+      name,
+      description,
+      birthdate,
       pet_type,
       pet_type_others,
-      pet_breed,
-      pet_breed_others,
-      pet_gender,
-      pet_weight,
+      breed,
+      breed_others,
+      gender,
+      weight_kg,
       pet_likes,
       pet_dislikes,
       pet_allergies,
       pet_vaccines,
-      pet_status,
+      status,
     } = matched;
 
     try {
       const pet = new Pet({
         owner_id: req.user._id,
-        name: pet_name,
-        description: pet_about,
-        birthdate: pet_birth,
+        name,
+        description,
+        birthdate,
         pet_type,
         pet_type_others,
-        breed: pet_breed,
-        breed_others: pet_breed_others,
-        gender: pet_gender,
-        status: pet_status,
+        breed,
+        breed_others,
+        gender,
+        status,
         pet_likes,
         pet_dislikes,
         pet_allergies,
         pet_vaccines,
-        weight_kg: pet_weight,
+        weight_kg,
         uploads: [],
       });
 
@@ -99,7 +99,120 @@ const petController = {
           res.send("success")
         }
     });
-  }
+  },
+
+  getEditPet: async (req, res) => {
+    try {
+      const pet = await Pet.findById(req.query.id)
+        .populate("uploads")
+        .lean()
+        .exec();
+      return res.render("edit-pet", { layout: false, inputs: pet });
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  },
+
+  postEditPet: async (req, res) => {
+    const errors = validationResult(req);
+    //const fileErrors = req.fileErrors;
+    const matched = matchedData(req, { locations: ["body"] });
+    console.log(matched)
+
+    if (!errors.isEmpty()) {
+      //const finalErrorsObj = { ...errors.mapped(), ...fileErrors };
+      const finalErrorsObj = errors.mapped()
+      return res.status(400).render("edit-pet", {
+        layout: false,
+        errors: finalErrorsObj,
+        inputs: matched,
+      });
+    }
+
+    // if no errors
+    const {
+      name,
+      description,
+      birthdate,
+      pet_type,
+      pet_type_others,
+      breed,
+      breed_others,
+      gender,
+      weight_kg,
+      pet_likes,
+      pet_dislikes,
+      pet_allergies,
+      pet_vaccines,
+      status,
+    } = matched;
+
+    try {
+      const pet = {
+        name: name,
+        description: description,
+        birthdate: birthdate,
+        pet_type: pet_type,
+        pet_type_others: pet_type_others,
+        breed: breed,
+        breed_others: breed_others,
+        gender: gender,
+        status: status,
+        pet_likes: pet_likes,
+        pet_dislikes: pet_dislikes,
+        pet_allergies: pet_allergies,
+        pet_vaccines: pet_vaccines,
+        weight_kg: weight_kg,
+        //uploads: [],
+      };
+
+     
+      // create uploads model
+      // const uploads = req.files.photos.map((photo) => {
+      //   const u = new Upload({
+      //     original_name: photo.originalname,
+      //     filename: photo.filename,
+      //     public_url: photo.publicUrl,
+      //   });
+      //   pet.uploads.push(u._id);
+      //   return u;
+      // });
+
+      // const docu_uploads = req.files.docus.map((docu) => {
+      //   const u = new Upload({
+      //     original_name: docu.originalname,
+      //     filename: docu.filename,
+      //     public_url: docu.publicUrl,
+      //   });
+      //   pet.docus.push(u._id);
+      //   return u;
+      // });
+
+      const doc = await Pet.findById(req.query.id)
+      const update = { name: name,
+        description: description,
+        birthdate: birthdate,
+        pet_type: pet_type,
+        pet_type_others: pet_type_others,
+        breed: breed,
+        breed_others: breed_others,
+        gender: gender,
+        status: status,
+        pet_likes: pet_likes,
+        pet_dislikes: pet_dislikes,
+        pet_allergies: pet_allergies,
+        pet_vaccines: pet_vaccines,
+        weight_kg: weight_kg };
+      await doc.updateOne(update);
+
+      // const uploadsResult = await Upload.bulkSave(uploads);
+      console.log("pet profile updated");
+      return res.redirect(`/profile/pet?id=${req.query.id}`);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  },
 };
 
 module.exports = petController;
