d3f8cf3a601d6a9b4787947197203eefe43aa634
Rethaniel Ramos <rethanielramos@gmail.com>
2022-06-22 20:18:59 +0800

    remove unused /profile/edit endpoint

--- routes/web/profile-routes.js
+++ routes/web/profile-routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { show, pet, getAllPets } = require("../../controllers/web/profile-controller");
+const {
+  show,
+  pet,
+  getAllPets,
+} = require("../../controllers/web/profile-controller");
 const ensureLoggedIn = require("../../middleware/ensure-logged-in");
 const profileRoutes = express.Router();
 
@@ -9,9 +13,6 @@ profileRoutes.route("/").get(show);
 
 profileRoutes.route("/pet").get(pet);
 
-profileRoutes.route("/all-pets").get(getAllPets); 
-
-// TODO: edit profile
-profileRoutes.route("/edit").get((req, res) => res.json(req.user));
+profileRoutes.route("/all-pets").get(getAllPets);
 
 module.exports = { router: profileRoutes, prefix: "/profile" };
